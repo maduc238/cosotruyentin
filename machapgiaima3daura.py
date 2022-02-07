fcfd893494db6dc0642daae2ad0766fa4ae51d25
Ma Duc <95759699+maduc238@users.noreply.github.com>
2022-02-07 16:42:33 +0700

    Add files via upload

---
+++ machapgiaima3daura.py
@@ -0,0 +1,46 @@
+import numpy as np
+from library import *
+from os import system
+system("cls")
+
+output = ['110','110','100','111','111','000']
+leng = len(output)
+
+g1 = np.array([[1,0,1]])
+g2 = np.array([[0,1,1]])
+g3 = np.array([[1,1,1]])
+
+def HammingDis(a,b):
+    dis = 0
+    for itr in range(len(a)):
+        if a[itr] != b[itr]: dis += 1
+    return dis
+
+Dismin = 100
+for i in range(1,2**leng):
+    A = np.zeros((1,len(g1[0])),dtype=int)
+    output_temp = []
+    temp = bin(i)[2:].zfill(leng)
+    HamDis = 0
+    for k in range(leng):
+        for j in range(1,len(A[0])):
+            A[0,len(A[0])-j] = A[0,len(A[0])-j-1]
+        A[0,0] = int(temp[k])
+        #nếu có g3 thì thêm
+        output_temp.append(str(DotBin(A,g1.T)[0][0]) + str(DotBin(A,g2.T)[0][0]) + str(DotBin(A,g3.T)[0][0]) )
+        HamDis += HammingDis(output[k],output_temp[k])
+    if HamDis < Dismin: Dismin = HamDis
+
+for i in range(1,2**leng):
+    A = np.zeros((1,len(g1[0])),dtype=int)
+    output_temp = []
+    temp = bin(i)[2:].zfill(leng)
+    HamDis = 0
+    for k in range(leng):
+        for j in range(1,len(A[0])):
+            A[0,len(A[0])-j] = A[0,len(A[0])-j-1]
+        A[0,0] = int(temp[k])
+        output_temp.append(str(DotBin(A,g1.T)[0][0]) + str(DotBin(A,g2.T)[0][0]) + str(DotBin(A,g3.T)[0][0]) )
+        HamDis += HammingDis(output[k],output_temp[k])
+    if Dismin == HamDis:
+        print("Input:",temp,". Output:",output_temp,". Hamming distance:",HamDis)
